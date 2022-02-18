8110ea88e7e55f89e44ecbce6d9f911e36bf77f2
alitafreshi <alitafreshi123@yahoo.com>
2022-02-18 15:11:26 +0330

    dependencies version updating

--- buildSrc/src/main/kotlin/ayan/Constance.kt
+++ buildSrc/src/main/kotlin/ayan/Constance.kt
@@ -1,7 +1,7 @@
 package ayan
 
 object Constance {
-    private const val ayanConstanceVersion = "0.0.6"
+    private const val ayanConstanceVersion = "0.0.8"
     const val ayanConstance =
         "com.github.alitafreshi.ayan-core:ayan-constance:${ayanConstanceVersion}"
 }
