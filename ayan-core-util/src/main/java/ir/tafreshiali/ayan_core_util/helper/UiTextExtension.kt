5f348abdf1a7900f7d75f3b2edae7907aca74635
ali tafreshi <alitafreshi123@yahoo.com>
2022-04-30 10:39:16 +0430

    Done

--- ayan-core-util/src/main/java/ir/tafreshiali/ayan_core_util/helper/UiTextExtension.kt
+++ ayan-core-util/src/main/java/ir/tafreshiali/ayan_core_util/helper/UiTextExtension.kt
@@ -1,5 +1,6 @@
 package ir.tafreshiali.ayan_core_util.helper
 
+import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import ir.tafreshiali.ayan_core.util.UiText
@@ -10,4 +11,12 @@ fun UiText.asString(): String = when (this) {
     is UiText.DynamicString -> value
 
     is UiText.StringResource -> stringResource(id = resId, *args)
+}
+
+
+fun UiText.asString(context: Context): String = when (this) {
+
+    is UiText.DynamicString -> value
+
+    is UiText.StringResource -> context.getString(resId, *args)
 }
